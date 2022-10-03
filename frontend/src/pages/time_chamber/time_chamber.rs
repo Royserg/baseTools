5d377b841c85e821de466231b69accaa8a442899
Kuba <staniojakub@gmail.com>
2022-10-04 02:34:13 +0300

    feat(fe): start countdown on btn click

--- frontend/src/pages/time_chamber/time_chamber.rs
+++ frontend/src/pages/time_chamber/time_chamber.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use yew::prelude::*;
 use yew_router::prelude::*;
 
@@ -23,121 +25,210 @@ pub fn time_chamber() -> Html {
 }
 
 // --- TIMER ---
-// enum TimerValueAction {
-//     IncrementSeconds,
-//     DecrementSeconds,
-//     IncrementMinutes,
-//     DecrementMinutes,
-//     Start,
-//     Pause,
-//     Stop,
-// }
-
-// #[derive(Default)]
-// struct SecondsState {
-//     minutes: usize,
-//     seconds: usize,
-// }
-
-#[derive(PartialEq)]
-pub enum TimerState {
+#[derive(Default, Debug, PartialEq, Copy, Clone)]
+enum TimerStatus {
+    #[default]
     Idle,
     Running,
     Stopped,
 }
 
+enum TimerStateAction {
+    // IncrementSeconds,
+    DecrementSeconds,
+    IncrementMinutes,
+    DecrementMinutes,
+    Start,
+    // Pause,
+    // Stop,
+}
+
+// #[derive(Default)]
+struct TimerState {
+    minutes: i32,
+    seconds: i32,
+    status: TimerStatus,
+}
+
+impl Default for TimerState {
+    fn default() -> Self {
+        TimerState {
+            minutes: 0,
+            seconds: 33,
+            status: TimerStatus::Idle,
+            // status: TimerStatus::Running,
+        }
+    }
+}
+
+impl Reducible for TimerState {
+    /// Reducer Action Type
+    type Action = TimerStateAction;
+
+    /// Reducer Function
+    fn reduce(self: Rc<Self>, action: Self::Action) -> Rc<Self> {
+        match action {
+            TimerStateAction::DecrementSeconds => {
+                let is_seconds_zero = self.seconds == 0;
+                let is_minutes_zero = self.minutes == 0;
+
+                if is_minutes_zero && is_seconds_zero {
+                    return Self {
+                        minutes: 0,
+                        seconds: 0,
+                        status: TimerStatus::Idle,
+                    }
+                    .into();
+                }
+
+                Self {
+                    status: self.status,
+                    seconds: if is_seconds_zero {
+                        59
+                    } else {
+                        self.seconds - 1
+                    },
+                    minutes: if is_seconds_zero {
+                        self.minutes - 1
+                    } else {
+                        self.minutes
+                    },
+                }
+                .into()
+            }
+            TimerStateAction::IncrementMinutes => Self {
+                status: self.status,
+                minutes: self.minutes + 1,
+                seconds: self.seconds,
+            }
+            .into(),
+            TimerStateAction::DecrementMinutes => Self {
+                status: self.status,
+                seconds: self.seconds,
+                minutes: if self.minutes != 0 {
+                    self.minutes - 1
+                } else {
+                    0
+                },
+            }
+            .into(),
+            TimerStateAction::Start => {
+                console_log!("GOT START ACTION");
+
+                Self {
+                    status: TimerStatus::Running,
+                    seconds: self.seconds,
+                    minutes: self.minutes,
+                }
+                .into()
+            }
+        }
+    }
+}
+
 #[function_component(Timer)]
 pub fn timer() -> Html {
-    let timer = use_state(|| 40);
-    let timer_state = use_state(|| TimerState::Idle);
+    let timer_state_handle = use_reducer(TimerState::default);
 
     let btn_classes = "rounded-md w-1/3 p-2 border text-2xl hover:bg-white hover:text-neutral-500";
 
-    let timer_interval: Interval;
+    use_effect({
+        let timer_state_handle = timer_state_handle.clone();
 
-    // use_effect_with_deps(callback, deps);
+        move || {
+            let mut interval: Option<Interval> = None;
 
-    // let current_timer_state = (*timer_state).clone();
-    use_effect_with_deps(
-        move |_| {
-            // let timer_state = timer_state.clone();
-            // let timer = timer.clone();
-            // let interval = Interval::new(1000, move || timer.set(*timer - 1));
-            move || ()
-        },
-        (),
-    );
-
-    // // console_log!("TIMER STATE CHANGED");
-    // timer_interval = Interval::new(1_000, move || {
-    //     timer.set(*timer - 1);
-    // })
-    // .forget();
-
-    // use_effect_with_deps(
-    // move |_| {
-    //         // ...
-    //         || ()
-    //     },
-    //     (), // dependents
-    // );
-
-    let action_btn_onclick = {
-        let timer_state = timer_state.clone();
-
-        Callback::from(move |_| match *timer_state {
-            TimerState::Idle => timer_state.set(TimerState::Running),
-            TimerState::Running => timer_state.set(TimerState::Stopped),
-            TimerState::Stopped => timer_state.set(TimerState::Idle),
-        })
-    };
-
-    let action_buttons = || -> Html {
-        match *timer_state {
-            TimerState::Idle => {
-                html! {
-                    <button
-                        class={btn_classes}
-                        onclick={action_btn_onclick}
-                    >
-                        {"Start"}
-                    </button>
-                }
+            if timer_state_handle.status == TimerStatus::Running {
+                interval = Some(Interval::new(1000, move || {
+                    timer_state_handle.dispatch(TimerStateAction::DecrementSeconds)
+                }));
             }
-            TimerState::Running => {
-                html! {
-                    <>
-                        <button
-                            class={btn_classes}
-                            onclick={action_btn_onclick}
-                        >
-                            {"Stop"}
-                        </button>
-                    </>
-                }
-            }
-            TimerState::Stopped => {
-                html! {
-                    <>
-                        <button
-                            class={btn_classes}
-                            onclick={action_btn_onclick}
-                        >
-                            {"Restart"}
-                        </button>
-                    </>
+
+            move || {
+                if let Some(i) = interval {
+                    drop(i)
                 }
             }
         }
+    });
+
+    let action_btn_onclick = {
+        let timer_state_handle = timer_state_handle.clone();
+
+        Callback::from(move |_| {
+            timer_state_handle.dispatch(TimerStateAction::Start);
+            console_log!("Button Clicked");
+        })
+        // let timer_state = timer_state.clone();
+
+        // Callback::from(move |_| match *timer_state {
+        //     TimerState::Idle => timer_state.set(TimerState::Running),
+        //     TimerState::Running => timer_state.set(TimerState::Stopped),
+        //     TimerState::Stopped => timer_state.set(TimerState::Idle),
+        // })
     };
 
-    // let timer = timer.clone();
+    let action_buttons = || -> Html {
+        html! {
+            <button
+                class={btn_classes}
+                onclick={action_btn_onclick}
+            >
+                {"Start"}
+            </button>
+        }
+        // match *timer_state {
+        //     TimerState::Idle => {
+        //         html! {
+        //             <button
+        //                 class={btn_classes}
+        //                 onclick={action_btn_onclick}
+        //             >
+        //                 {"Start"}
+        //             </button>
+        //         }
+        //     }
+        //     TimerState::Running => {
+        //         html! {
+        //             <>
+        //                 <button
+        //                     class={btn_classes}
+        //                     onclick={action_btn_onclick}
+        //                 >
+        //                     {"Stop"}
+        //                 </button>
+        //             </>
+        //         }
+        //     }
+        //     TimerState::Stopped => {
+        //         html! {
+        //             <>
+        //                 <button
+        //                     class={btn_classes}
+        //                     onclick={action_btn_onclick}
+        //                 >
+        //                     {"Restart"}
+        //                 </button>
+        //             </>
+        //         }
+        //     }
+        // }
+    };
+
+    // -- Util function --
+    let pad_int_with_zero = |val: i32| -> String {
+        if val < 10 {
+            return format!("0{}", val.to_string());
+        }
+        val.to_string()
+    };
+
     html! {
         <div class="h-full flex grow flex-col justify-center items-center text-center">
             <div class="text-9xl">
-                <span>{*timer}</span>
+                <span>{pad_int_with_zero(timer_state_handle.minutes)}</span>
                 {":"}
-                <span>{"00"}</span>
+                <span>{pad_int_with_zero(timer_state_handle.seconds)}</span>
             </div>
 
             // Divider
