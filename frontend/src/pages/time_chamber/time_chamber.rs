be8db42b0526cc9874cc1fdfc7fe08e40abec562
Kuba <staniojakub@gmail.com>
2022-10-03 22:44:30 +0300

    style(fe): add skeleton for the Timer component (no timeout)

--- frontend/src/pages/time_chamber/time_chamber.rs
+++ frontend/src/pages/time_chamber/time_chamber.rs
@@ -2,6 +2,8 @@ use yew::prelude::*;
 use yew_router::prelude::*;
 
 use crate::Route;
+use gloo_timers::callback::Interval;
+use weblog::console_log;
 
 #[function_component(TimeChamber)]
 pub fn time_chamber() -> Html {
@@ -9,9 +11,9 @@ pub fn time_chamber() -> Html {
 
     let onclick = Callback::once(move |_| history.push(Route::Home));
     html! {
-        <div class="rounded-b-xl bg-neutral-500 h-96">
-            <h1 class="text-5xl text-center">{ "Time Chamber" }</h1>
-            <div class="flex ">
+        <div class="rounded-b-xl bg-neutral-500 flex flex-col grow pt-4 overflow-hidden">
+            <h1 class="text-4xl text-center underline">{ "Time Chamber" }</h1>
+            <div class="flex">
                 <button {onclick} class="rounded-md p-2 ml-4 -mt-10 bg-slate-800">{ "Go Back" }</button>
             </div>
 
@@ -20,14 +22,130 @@ pub fn time_chamber() -> Html {
     }
 }
 
+// --- TIMER ---
+// enum TimerValueAction {
+//     IncrementSeconds,
+//     DecrementSeconds,
+//     IncrementMinutes,
+//     DecrementMinutes,
+//     Start,
+//     Pause,
+//     Stop,
+// }
+
+// #[derive(Default)]
+// struct SecondsState {
+//     minutes: usize,
+//     seconds: usize,
+// }
+
+#[derive(PartialEq)]
+pub enum TimerState {
+    Idle,
+    Running,
+    Stopped,
+}
+
 #[function_component(Timer)]
 pub fn timer() -> Html {
-    let counter = use_state(|| 20);
+    let timer = use_state(|| 40);
+    let timer_state = use_state(|| TimerState::Idle);
 
+    let btn_classes = "rounded-md w-1/3 p-2 border text-2xl hover:bg-white hover:text-neutral-500";
+
+    let timer_interval: Interval;
+
+    // use_effect_with_deps(callback, deps);
+
+    // let current_timer_state = (*timer_state).clone();
+    use_effect_with_deps(
+        move |_| {
+            // let timer_state = timer_state.clone();
+            // let timer = timer.clone();
+            // let interval = Interval::new(1000, move || timer.set(*timer - 1));
+            move || ()
+        },
+        (),
+    );
+
+    // // console_log!("TIMER STATE CHANGED");
+    // timer_interval = Interval::new(1_000, move || {
+    //     timer.set(*timer - 1);
+    // })
+    // .forget();
+
+    // use_effect_with_deps(
+    // move |_| {
+    //         // ...
+    //         || ()
+    //     },
+    //     (), // dependents
+    // );
+
+    let action_btn_onclick = {
+        let timer_state = timer_state.clone();
+
+        Callback::from(move |_| match *timer_state {
+            TimerState::Idle => timer_state.set(TimerState::Running),
+            TimerState::Running => timer_state.set(TimerState::Stopped),
+            TimerState::Stopped => timer_state.set(TimerState::Idle),
+        })
+    };
+
+    let action_buttons = || -> Html {
+        match *timer_state {
+            TimerState::Idle => {
+                html! {
+                    <button
+                        class={btn_classes}
+                        onclick={action_btn_onclick}
+                    >
+                        {"Start"}
+                    </button>
+                }
+            }
+            TimerState::Running => {
+                html! {
+                    <>
+                        <button
+                            class={btn_classes}
+                            onclick={action_btn_onclick}
+                        >
+                            {"Stop"}
+                        </button>
+                    </>
+                }
+            }
+            TimerState::Stopped => {
+                html! {
+                    <>
+                        <button
+                            class={btn_classes}
+                            onclick={action_btn_onclick}
+                        >
+                            {"Restart"}
+                        </button>
+                    </>
+                }
+            }
+        }
+    };
+
+    // let timer = timer.clone();
     html! {
-        <div class="w-100 text-center">
-            <h1>{"Timer"}</h1>
-            <div>{format!("{}", *counter)}</div>
+        <div class="h-full flex grow flex-col justify-center items-center text-center">
+            <div class="text-9xl">
+                <span>{*timer}</span>
+                {":"}
+                <span>{"00"}</span>
+            </div>
+
+            // Divider
+            <div class="my-10"></div>
+
+            <div class="w-full">
+                {action_buttons()}
+            </div>
         </div>
     }
 }
