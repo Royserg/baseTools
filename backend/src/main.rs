ef05f873c2e989a91b44dbd2654e83286ce6c7c6
Kuba <staniojakub@gmail.com>
2022-10-08 20:48:55 +0300

    feat(be): hide tray-menu when it loses focus

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,6 +9,7 @@ use tauri::{
 
 // --- WINDOWS ---
 const MAIN_WINDOW_LABEL: &str = "main";
+const TRAY_WINDOW_LABEL: &str = "tray-menu";
 // --- TRAY ITEMS ---
 const TRAY_ITEM_OPEN_APP_ID: &str = "open_app";
 const TRAY_ITEM_QUIT_ID: &str = "quit";
@@ -25,7 +26,24 @@ fn main() {
         })
         .system_tray(app_tray)
         .on_system_tray_event(app_tray_event_handler)
-        .invoke_handler(tauri::generate_handler![hide_window])
+        .invoke_handler(tauri::generate_handler![
+            hide_main_window,
+            show_main_window,
+            quit_app
+        ])
+        // --- Window events
+        .on_window_event(|event| match event.event() {
+            tauri::WindowEvent::Focused(focused) => {
+                // Close tray-menu when window loses focus
+                if !focused {
+                    if event.window().label() == TRAY_WINDOW_LABEL {
+                        event.window().close().unwrap();
+                    }
+                }
+            }
+            _ => {}
+        })
+        // --- Build
         .build(tauri::generate_context!())
         .expect("error while running tauri application")
         .run(|_app_handle, event| match event {
