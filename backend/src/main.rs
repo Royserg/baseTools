0ba400fd1feee86dca702c7c68b4ce1268a9d0f5
Kuba <staniojakub@gmail.com>
2022-10-08 13:57:39 +0300

    chore(be): hide main window by default, add configs for tray menu

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,8 +1,7 @@
-#![cfg_attr(
+#[cfg_attr(
     all(not(debug_assertions), target_os = "windows"),
     windows_subsystem = "windows"
 )]
-
 use tauri::{
     AppHandle, Builder, CustomMenuItem, Manager, SystemTray, SystemTrayEvent, SystemTrayMenu,
     SystemTrayMenuItem, Window,
@@ -20,39 +19,67 @@ fn main() {
     Builder::default()
         .setup(|app| {
             let _window = app.get_window(MAIN_WINDOW_LABEL).unwrap();
+            _window.hide().unwrap();
 
             Ok(())
         })
         .system_tray(app_tray)
         .on_system_tray_event(app_tray_event_handler)
         .invoke_handler(tauri::generate_handler![hide_window])
-        .run(tauri::generate_context!())
+        .build(tauri::generate_context!())
         .expect("error while running tauri application")
+        .run(|_app_handle, event| match event {
+            tauri::RunEvent::ExitRequested { api, .. } => {
+                api.prevent_exit();
+            }
+            _ => {}
+        });
 }
 
 // --- System App Tray ---
 fn init_app_tray() -> SystemTray {
     let open_app = CustomMenuItem::new(TRAY_ITEM_OPEN_APP_ID, "Open App");
     let quit = CustomMenuItem::new(TRAY_ITEM_QUIT_ID, "Quit");
-    let tray_menu = SystemTrayMenu::new()
+    let _tray_menu = SystemTrayMenu::new()
         .add_item(open_app)
         .add_native_item(SystemTrayMenuItem::Separator)
         .add_item(quit);
 
-    SystemTray::new()
-        .with_id("tray")
-        .with_menu_on_left_click(false)
-        .with_menu(tray_menu)
+    SystemTray::new().with_id("tray")
+    // .with_menu(tray_menu)
+    // .with_menu_on_left_click(false)
 }
 
 fn app_tray_event_handler(app: &AppHandle, event: SystemTrayEvent) {
     match event {
         SystemTrayEvent::LeftClick {
-            position: _,
-            size: _,
-            ..
+            position, size: _, ..
         } => {
             println!("system tray received a LEFT click");
+
+            let window_width = 300.00;
+            let window_height = 200.00;
+
+            let window = tauri::WindowBuilder::new(
+                app,
+                "tray-menu", /* the unique window label */
+                tauri::WindowUrl::App("tray-menu".into()),
+            )
+            .inner_size(window_width, window_height)
+            .position(position.x - (window_width / 2.0), position.y)
+            .decorations(true)
+            .resizable(false)
+            .decorations(false)
+            .skip_taskbar(true)
+            .transparent(true);
+
+            if let Ok(window) = window.build() {
+                window
+                    .set_focus()
+                    .expect("Failed to set focus on the window");
+            } else {
+                println!("Window already exists.");
+            }
         }
         SystemTrayEvent::RightClick {
             position: _,
@@ -77,5 +104,6 @@ fn app_tray_event_handler(app: &AppHandle, event: SystemTrayEvent) {
 
 #[tauri::command]
 fn hide_window(window: Window) {
-    window.hide().unwrap();
+    // window.hide().unwrap();
+    window.close().unwrap();
 }
